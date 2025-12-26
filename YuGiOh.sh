9e0aaae3142dc24d45c08efa0d52955a92813c2e
Muhammad Habbal <ufnmz@student.kit.edu>
2025-12-27 00:27:44 +0100

    Moving all the progress to a Repo
    I Moved all the work i did till now to the Repo, next im gonna clean the repo and comment the code before i continue the project.

---
+++ YuGiOh.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo -ne '\033c\033]0;YuGiOh\a'
+base_path="$(dirname "$(realpath "$0")")"
+"$base_path/YuGiOh.x86_32" "$@"
---
+++ cardManeger.gd
@@ -0,0 +1,110 @@
+extends Node2D
+
+
+const COLLUSION_MASK_CARD = 1
+var screen_size
+var card_being_dragged
+var is_hovering_over_card
+
+
+
+	
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	
+	
+func _process(delta: float) -> void:
+	if card_being_dragged:
+		var mouse_pos = get_global_mouse_position()
+		card_being_dragged.position = Vector2(clamp(mouse_pos.x, 0, screen_size.x), 
+		clamp(mouse_pos.y, 0, screen_size.y) )
+
+
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			var card = raycast_check_for_card()
+			if card:
+				start_drag(card)
+		else: 
+			if card_being_dragged:
+				finish_drag()
+
+			
+			
+func start_drag(card):
+	card_being_dragged = card
+	card.scale = Vector2(1,1)
+
+			
+func finish_drag():
+	if card_being_dragged:
+		card_being_dragged.scale = Vector2(1.10,1.10)
+		card_being_dragged = null
+
+
+
+
+func connect_card_signals(card):
+	card.connect("hovered", on_hoverd_over_card)
+	card.connect("hovered_off", on_hoverd_off_card)
+
+
+
+func on_hoverd_over_card(card):
+	if !is_hovering_over_card:
+		is_hovering_over_card = true
+		highlilght_card(card, true)
+		
+		
+func on_hoverd_off_card(card):
+		highlilght_card(card, false)
+		var new_card_hovered = raycast_check_for_card()
+		if new_card_hovered:
+			highlilght_card(new_card_hovered,true)
+		else:
+			is_hovering_over_card = false
+			
+		
+		
+func highlilght_card(card, hovered):
+	if hovered:
+		card.scale = Vector2(1.10,1.10)
+
+		card.z_index = 2
+	else:
+		card.scale = Vector2(1,1)
+
+		card.z_index = 1
+	
+			
+func raycast_check_for_card():
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = 1
+	var result = space_state.intersect_point(parameters)	
+	if result.size() > 0:
+		
+		return get_card_with_highest_z_index(result)
+	return null	
+	
+	
+	
+func get_card_with_highest_z_index(cards):
+	var highest_z_card = cards[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+	
+	
+	for i in range(1,cards.size()):
+		var current_card = cards[1].collider.get_parent()
+		if current_card.z_index > highest_z_index:
+			highest_z_card = current_card
+			highest_z_index = current_card.z_index
+			
+	return highest_z_card
+		
+	
+	
